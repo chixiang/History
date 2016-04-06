cb3de6363a107f6ef454b953a08258f128eb5be4
chixiang <chixiang1983@gmail.com>
2016-04-06 21:29:09 +0800

    优化newhis页面再次提交

--- widget/script/index.js
+++ widget/script/index.js
@@ -1,202 +1,99 @@
+/**
+ * [apiready description]
+ * @return {[type]} [description]
+ */
 var apiready = function() {
-    $api.fixStatusBar($api.dom('header'));
-    var UIListView;
+    $api.fixIos7Bar($api.dom('header'));
     var listViewData = [];
-    var model = api.require('model');
-    var query = api.require('query');
+    per_page_num = 10;
+    v_loaded_recors = per_page_num;
+    model = api.require('model');
+    query = api.require('query');
     model.config({
         appId: 'A6903478274381',
         appKey: '460A4799-0424-A29B-6809-F06FDF1D888F',
         host: 'https://d.apicloud.com'
     });
-    query.createQuery(function(ret, err) {
-        if (ret && ret.qid) {
-            model.findAll({
-                    class: 'history',
-                    qid: ret.id
-                },
-                function(ret, err) {
-                    var idx = 0;
-                    while (ret[idx] != undefined) {
-                        var listViewItem = {
-                            uid: ret[idx].id,
-                            title: ret[idx].name,
-                            subTitle: ret[idx].disease,
-                            remark: ret[idx].comeDate
-                        };
-                        listViewData.push(listViewItem);
-                        idx = idx + 1;
-                    }
-                    UIListView = api.require('UIListView');
-                    UIListView.open({
-                        rect: {
-                            x: 0,
-                            y: $api.dom('header').offsetHeight,
-                            w: api.winWidth,
-                            h: $api.dom('#main').offsetHeight
-                        },
-                        data: listViewData,
-                        rightBtns: [{
-                            bgColor: '#888', //（可选项）字符串类型；按钮背景色，支持rgb、rgba、#；默认：'#388e8e'
-                            activeBgColor: '', //（可选项）字符串类型；按钮按下时的背景色，支持rgb、rgba、#
-                            width: 70, //（可选项）数字类型；按钮的宽度；默认：w / 4
-                            title: '修改', //（可选项）字符串类型；按钮标题，水平、垂直居中
-                            titleSize: 12, //（可选项）数字类型；按钮标题文字大小；默认：12
-                            titleColor: '#fff', //（可选项）字符串类型；按钮标题文字颜色，支持rgb、rgba、#；默认：'#ffffff'
-                            icon: '', //（可选项）字符串类型；按钮标题前的图标路径（本地路径，支持fs://，widget://），水平、垂直居中，图标为正方形
-                            iconWidth: 20 //（可选项）数字类型；按钮标题前的图标宽度，图标为正方形；默认：20
-                        }, {
-                            bgColor: '#888', //（可选项）字符串类型；按钮背景色，支持rgb、rgba、#；默认：'#388e8e'
-                            activeBgColor: '', //（可选项）字符串类型；按钮按下时的背景色，支持rgb、rgba、#
-                            width: 70, //（可选项）数字类型；按钮的宽度；默认：w / 4
-                            title: '删除', //（可选项）字符串类型；按钮标题，水平、垂直居中
-                            titleSize: 12, //（可选项）数字类型；按钮标题文字大小；默认：12
-                            titleColor: '#fff', //（可选项）字符串类型；按钮标题文字颜色，支持rgb、rgba、#；默认：'#ffffff'
-                            icon: '', //（可选项）字符串类型；按钮标题前的图标路径（本地路径，支持fs://，widget://），水平、垂直居中，图标为正方形
-                            iconWidth: 20 //（可选项）数字类型；按钮标题前的图标宽度，图标为正方形；默认：20
-                        }],
-                        styles: {
-                            borderColor: '#696969',
-                            item: {
-                                bgColor: '#eee',
-                                activeBgColor: '#F5F5F5',
-                                height: 55.0,
-                                imgWidth: 40,
-                                imgHeight: 40,
-                                imgCorner: 4,
-                                placeholderImg: '',
-                                titleSize: 12.0,
-                                titleColor: '#000',
-                                subTitleSize: 12.0,
-                                subTitleColor: '#000',
-                                remarkColor: '#000',
-                                remarkSize: 16,
-                                remarkIconWidth: 30
-                            }
-                        },
-                        fixedOn: api.frameName
-                    }, function(ret, err) {
-                        // if( ret ){
-                        //      alert( JSON.stringify( ret ) );
-                        // }else{
-                        //      alert( JSON.stringify( err ) );
-                        // }
-                        api.toast({
-                            msg: ret,
-                            duration: 2000,
-                            location: "top"
-                        });
-                    });
-                    UIListView.setRefreshHeader({
-                            bgColor: "#f5f5f5",
-                            loadingImg: "image/bottombtn0301.png"
-                        },
-                        function(ret, err) {
-                            var model = api.require('model');
-                            var query = api.require('query');
-                            model.config({
-                                appId: 'A6903478274381',
-                                appKey: '460A4799-0424-A29B-6809-F06FDF1D888F',
-                                host: 'https://d.apicloud.com'
-                            });
-                            query.createQuery(function(ret, err) {
-                                if (ret && ret.qid) {
-                                    model.findAll({
-                                            class: 'history',
-                                            qid: ret.id
-                                        },
-                                        function(ret, err) {
-                                            var idx = 0;
-                                            listViewData = [];
-                                            while (ret[idx] != undefined) {
-                                                var listViewItem = {
-                                                    uid: ret[idx].id,
-                                                    title: ret[idx].name,
-                                                    subTitle: ret[idx].disease,
-                                                    remark: ret[idx].comeDate
-                                                };
-                                                listViewData.push(listViewItem);
-                                                idx = idx + 1;
-                                            }
-                                            UIListView.reloadData({
-                                                data: listViewData
-                                            })
-                                        })
-                                }
-                            })
-                        });
-                    UIListView.setRefreshFooter({
-                            bgColor: "#f5f5f5",
-                            loadingImg: "image/bottombtn0301.png"
-                        },
-                        function(ret, err) {
-                            /* 触发加载事件, 此处应使用使用 appendData 方法.*/
-                            // UIListView.appendData({
-                            //     data: [{
-                            //         title: "新标题3",
-                            //         subTitle: "新子标题3"
-                            //     }, {
-                            //         title: "新标题4",
-                            //         subTitle: "新子标题4"
-                            //     }]
-                            // });
-                            pull_up_load_more(UIListView);
-                        });
-                }
-            );
-        }
+    UIListView = api.require('UIListView');
+    UIListView.open({
+        rect: {
+            x: 0,
+            y: $api.dom('header').offsetHeight,
+            w: api.winWidth,
+            h: $api.dom('#main').offsetHeight
+        },
+        data: listViewData,
+        rightBtns: [{
+            bgColor: '#EED6B0', //（可选项）字符串类型；按钮背景色，支持rgb、rgba、#；默认：'#388e8e'
+            activeBgColor: '', //（可选项）字符串类型；按钮按下时的背景色，支持rgb、rgba、#
+            width: 70, //（可选项）数字类型；按钮的宽度；默认：w / 4
+            title: '修改', //（可选项）字符串类型；按钮标题，水平、垂直居中
+            titleSize: 14, //（可选项）数字类型；按钮标题文字大小；默认：12
+            titleColor: '#655A55', //（可选项）字符串类型；按钮标题文字颜色，支持rgb、rgba、#；默认：'#ffffff'
+            icon: '', //（可选项）字符串类型；按钮标题前的图标路径（本地路径，支持fs://，widget://），水平、垂直居中，图标为正方形
+            iconWidth: 20 //（可选项）数字类型；按钮标题前的图标宽度，图标为正方形；默认：20
+        }, {
+            bgColor: '#EED6B0', //（可选项）字符串类型；按钮背景色，支持rgb、rgba、#；默认：'#388e8e'
+            activeBgColor: '', //（可选项）字符串类型；按钮按下时的背景色，支持rgb、rgba、#
+            width: 70, //（可选项）数字类型；按钮的宽度；默认：w / 4
+            title: '删除', //（可选项）字符串类型；按钮标题，水平、垂直居中
+            titleSize: 14, //（可选项）数字类型；按钮标题文字大小；默认：12
+            titleColor: '#655A55', //（可选项）字符串类型；按钮标题文字颜色，支持rgb、rgba、#；默认：'#ffffff'
+            icon: '', //（可选项）字符串类型；按钮标题前的图标路径（本地路径，支持fs://，widget://），水平、垂直居中，图标为正方形
+            iconWidth: 20 //（可选项）数字类型；按钮标题前的图标宽度，图标为正方形；默认：20
+        }],
+        styles: {
+            borderColor: '#EED6B0',
+            item: {
+                bgColor: '#F7F3EA',
+                activeBgColor: '#F5F5F5',
+                height: 55.0,
+                imgWidth: 40,
+                imgHeight: 40,
+                imgCorner: 4,
+                placeholderImg: '',
+                titleSize: 12.0,
+                titleColor: '#655A55',
+                subTitleSize: 12.0,
+                subTitleColor: '#E1D7CA',
+                remarkColor: '#655A55',
+                remarkSize: 14,
+                remarkIconWidth: 30
+            }
+        },
+        fixedOn: api.frameName
+    }, function(ret, err) {
+        pull_down_reload(UIListView);
     });
+    UIListView.setRefreshHeader({
+            bgColor: "#f5f5f5",
+            loadingImg: "image/bottombtn0301.png"
+        },
+        function(ret, err) {
+            pull_down_reload();
+        });
+
+    UIListView.setRefreshFooter({
+            bgColor: "#f5f5f5",
+            loadingImg: "image/bottombtn0301.png"
+        },
+        function(ret, err) {
+            pull_up_load_more();
+        });
+
 }
 
-function getAllMyHis(hislist, callback) {
-    var model = api.require('model');
-    var query = api.require('query');
-    model.config({
-        appId: 'A6903478274381',
-        appKey: '460A4799-0424-A29B-6809-F06FDF1D888F',
-        host: 'https://d.apicloud.com'
-    });
-    query.createQuery(function(ret, err) {
-        if (ret && ret.qid) {
-            model.findAll({
-                    class: 'history',
-                    qid: ret.id
-                },
-                function(ret, err) {
-                    var idx = 0;
-                    hislist = [];
-                    while (ret[idx] != undefined) {
-                        var listViewItem = {
-                            uid: ret[idx].id,
-                            title: ret[idx].name,
-                            subTitle: ret[idx].disease,
-                            remark: ret[idx].comeDate
-                        };
-                        hislist.push(listViewItem);
-                        idx = idx + 1;
-                    }
-                    if (callback) {
-                        alert("hislist" + JSON.stringify(hislist));
-                        callback();
-                    }
-                })
-        }
-    })
-}
-
-function pull_up_load_more(ui_list_view) {
-    var model = api.require('model');
-    var query = api.require('query');
+/**
+ * [pull_down_reload description]
+ * @return {[type]} [description]
+ */
+function pull_down_reload() {
     query.createQuery(function(ret, err) {
         if (ret && ret.qid) {
             var queryId = ret.qid;
             query.limit({
                 qid: queryId,
-                value: 5
-            });
-            query.skip({
-                qid: queryId,
-                value: 5
+                value: per_page_num
             });
             // query.justFields({
             //     qid: queryId,
@@ -208,7 +105,7 @@ function pull_up_load_more(ui_list_view) {
             // });
             model.findAll({
                 class: "history",
-                qid: ret.id
+                qid: queryId
             }, function(ret, err) {
                 if (err) {
                     alert(JSON.stringify(err));
@@ -227,26 +124,115 @@ function pull_up_load_more(ui_list_view) {
                             idx = idx + 1;
                         }
                     }
-                    appendData(ui_list_view, json_objs);
+                    reloadData(json_objs);
+                }
+            });
+        }
+    })
+
+}
+
+/**
+ * [pull_up_load_more description]
+ * @return {[type]} [description]
+ */
+function pull_up_load_more() {
+    query.createQuery(function(ret, err) {
+        if (ret && ret.qid) {
+            var queryId = ret.qid;
+            query.limit({
+                qid: queryId,
+                value: per_page_num
+            });
+            query.skip({
+                qid: queryId,
+                value: v_loaded_recors
+            });
+            // query.justFields({
+            //     qid: queryId,
+            //     value: ["id", "msg", "updatedAt"]
+            // });
+            // query.desc({
+            //     qid: queryId,
+            //     column: "updatedAt"
+            // });
+            model.findAll({
+                class: "history",
+                qid: queryId
+            }, function(ret, err) {
+                if (err) {
+                    alert(JSON.stringify(err));
+                } else {
+                    if (ret) {
+                        var idx = 0;
+                        var json_objs = [];
+                        while (ret[idx] != undefined) {
+                            var listViewItem = {
+                                uid: ret[idx].id,
+                                title: ret[idx].name,
+                                subTitle: ret[idx].disease,
+                                remark: ret[idx].comeDate
+                            };
+                            json_objs.push(listViewItem);
+                            idx = idx + 1;
+                        }
+                    }
+                    appendData(json_objs);
                 }
             });
         }
     })
 }
 
-function appendData(ui_list_view, json_objs) {
-    ui_list_view.appendData({
+/**
+ * [reloadData description]
+ * @param  {[type]} json_objs [description]
+ * @return {[type]}           [description]
+ */
+function reloadData(json_objs) {
+    UIListView.reloadData({
             "data": json_objs
         },
         function(ret) {
             if (ret.status) {
                 api.toast({
-                    msg: '追加数据成功'
+                    msg: '加载数据成功'
                 });
+                v_loaded_recors = per_page_num;
             }
-        });
+        })
 }
 
+/**
+ * [appendData description]
+ * @param  {[type]} json_objs [description]
+ * @return {[type]}           [description]
+ */
+function appendData(json_objs) {
+    if (json_objs[0] != undefined) {
+        UIListView.appendData({
+                "data": json_objs
+            },
+            function(ret) {
+                if (ret.status) {
+                    api.toast({
+                        msg: '追加数据成功'
+                    });
+                    v_loaded_recors = v_loaded_recors + per_page_num;
+                }
+            });
+    } else {
+        api.toast({
+            msg: '没有更多数据了'
+        });
+    }
+}
+
+/**
+ * [openWin description]
+ * @param  {[type]} type [description]
+ * @return {[type]}      [description]
+ */
 function openWin(type) {
     api.openWin({
         name: type,
@@ -257,6 +243,10 @@ function openWin(type) {
     });
 }
 
+/**
+ * [openSearch description]
+ * @return {[type]} [description]
+ */
 function openSearch() {
     var UISearchBar = api.require('UISearchBar');
     UISearchBar.open({
