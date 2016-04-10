a569f87346a193bcd0fed30e29d2fbce71ef063d
chixiang <chixiang1983@gmail.com>
2016-04-10 16:17:02 +0800

    使用util.js中的方法设置patient页面值，修改生日截取前10位“ ” “

--- widget/script/util.js
+++ widget/script/util.js
@@ -7,3 +7,16 @@ function getDate() {
     var formatDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
     return formatDate.replace(/\b(\w)\b/g, '0$1');
 }
+
+function setPatient(data) {
+    alert("setPatient");
+    $api.byId('name').value = data.name;
+    $api.byId('gender').value = data.gender ? "男" : "女";
+    $api.byId('birthday').value = data.birthday.substring(0, 10);
+    $api.byId('age').value = data.age;
+    $api.byId('admission_number').value = data.admission_number;
+    $api.byId('outpatient_number').value = data.outpatient_number;
+    $api.byId('phone').value = data.phone;
+    $api.byId('address').value = data.address;
+    $api.byId('job').value = data.job;
+}
