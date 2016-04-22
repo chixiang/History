ef145c914adac721a03215fc894cd3254aaccde7
chixiang <chixiang1983@gmail.com>
2016-04-23 01:51:03 +0800

    修改为openWin再openFrame的方式，提高Android性能

--- widget/script/historyList.js
+++ widget/script/historyList.js
@@ -323,7 +323,7 @@ function openWin(type) {
 function viewHistory(history_id) {
     api.openWin({
         name: "history",
-        url: './html/viewFullHistory.html',
+        url: './html/viewFullHistoryWin.html',
         pageParam: {
             history_id: history_id
         },
